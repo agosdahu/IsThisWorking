836a398b6f0badb1a8b9aa2a30ceaa00b35c238f
David Agoston <david.agoston@bbraun.com>
2020-03-18 11:23:39 +0100

    gitlens shit is weeded out

--- main.c
+++ main.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
-<<<<<<< HEAD
+
 #include "sub2.h"
-=======
 #include "sub.h"
->>>>>>> a638d9041da731405b96297baf47ae8f44a5ffef
 
 int main()
 {
     printf("HelloWorld!\n And MR. Linux\n\n");
-<<<<<<< HEAD
     printf("%i", sub2());
-=======
     sub();
     printf("Hey, this is Bullshit");
->>>>>>> a638d9041da731405b96297baf47ae8f44a5ffef
+
     return 0;
 }
